83055e8a8b5b92741c802d434479fc67c1ff11b9
Clif Reeder <clif@voxmedia.com>
2014-11-29 00:09:27 -0500

    lol no more non blocking

--- thumbor_webdav_result_storage/__init__.py
+++ thumbor_webdav_result_storage/__init__.py
@@ -3,7 +3,7 @@
 from thumbor.result_storages import BaseStorage
 from thumbor.utils import logger
 
-from tornado.httpclient import AsyncHTTPClient
+from tornado.httpclient import HTTPClient
 
 import hashlib
 import os
@@ -21,31 +21,27 @@ class Storage(BaseStorage):
       normalized_path = self.normalize_path(self.context.request.url)
       uri = self.context.config.get('RESULT_STORAGE_WEBDAV_HOST') + normalized_path
       logger.debug("[RESULT_STORAGE] Making PUT request to: %s", uri)
-      http_client = AsyncHTTPClient()
-      http_client.fetch(uri, _handle_put_request, method='PUT')
-
-  def _handle_put_request(response):
-    if response.error:
-        logger.error("[RESULT_STORAGE] Error on PUT request: %s", response.error)
-        return None
-    else:
-        logger.debug("[RESULT_STORAGE] Success on PUT request!")
-        return None
+      http_client = HTTPClient()
+      response = http_client.fetch(uri, _handle_put_request, method='PUT')
+      if response.error:
+          logger.error("[RESULT_STORAGE] Error on PUT request: %s", response.error)
+          return None
+      else:
+          logger.debug("[RESULT_STORAGE] Success on PUT request!")
+          return None
 
   def get(self):
       normalized_path = self.normalize_path(self.context.request.url)
       uri = self.context.config.get('RESULT_STORAGE_WEBDAV_HOST') + normalized_path
       logger.debug("[RESULT_STORAGE] Making GET request to: %s", uri)
-      http_client = AsyncHTTPClient()
-      http_client.fetch(uri, _handle_get_request)
-
-  def _handle_get_request(response):
-    if response.error:
-        logger.debug("[RESULT_STORAGE] Error on GET request: %s", response.error)
-        return None
-    else:
-        logger.debug("[RESULT_STORAGE] Success on GET request!")
-        return response.body
+      http_client = HTTPClient()
+      response = http_client.fetch(uri, _handle_get_request)
+      if response.error:
+          logger.debug("[RESULT_STORAGE] Error on GET request: %s", response.error)
+          return None
+      else:
+          logger.debug("[RESULT_STORAGE] Success on GET request!")
+          return response.body
 
   def normalize_path(self, path):
       root_path = self.context.config.get('RESULT_STORAGE_WEBDAV_ROOT_PATH', default='/')
